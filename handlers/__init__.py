ecb64af19f0ed989b15c6a93228aae59b523cc37
bobberdolle1 <kirillkrovyakov@yandex.com>
2025-10-18 20:58:40 +0300

    litte fixes

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,5 @@
 """Handlers package for EFT Helper bot."""
 from . import common, search, builds, loyalty, tier_list, settings
+from . import community_builds, dynamic_builds
 
-__all__ = ["common", "search", "builds", "loyalty", "tier_list", "settings"]
+__all__ = ["common", "search", "builds", "loyalty", "tier_list", "settings", "community_builds", "dynamic_builds"]
